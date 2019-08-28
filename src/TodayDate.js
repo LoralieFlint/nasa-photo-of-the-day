edbfe87fa36a53166fbb75f8029502aefc6fe1fb
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-08-27 21:53:06 -0600

    met MVP :)

---
+++ src/TodayDate.js
@@ -0,0 +1,13 @@
+// import React from "react"
+
+// function date() {
+//     let today = document.createElement("p");
+//         today.textContent = new Date().toString(" ");
+
+//         return {
+//             <div>
+//             <today />
+//             </div>
+//         }
+// }
+// export default today;
